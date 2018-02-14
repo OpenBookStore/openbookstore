0a0d085cd61c61367c2d281e050aef448cf1a527
vindarel <ehvince@mailz.org>
2018-02-15 00:18:25 +0100

    Book class. Return Book objects. Pretty print.

--- src/bookshops.lisp
+++ src/bookshops.lisp
@@ -11,6 +11,29 @@
 (defparameter *datasource* *french-search*
   "The default data source.")
 
+(defclass book ()
+  ((datasource :accessor datasource :initarg :datasource)
+   (title :accessor title :initarg :title)
+   (price :accessor price :initarg :price)
+   (date-publication :accessor date-publication :initarg :date-publication)
+   (editor :accessor editor :initarg :editor)
+   (authors :accessor authors :initarg :authors)))
+
+(defmethod print-object ((book book) stream)
+  (print-unreadable-object (book stream :type t)
+    (with-accessors ((title title)
+                     (price price)
+                     (authors authors))
+        book
+      (format stream "~a, ~a" title authors))))
+
+(defclass author ()
+  ((name :accessor name :initarg :name)))
+
+(defmethod print-object ((author author) stream)
+  (print-unreadable-object (author stream :type t)
+    (format stream "~a, ~a" (slot-value author 'name))))
+
 (defun get-url (url)
   "Http get this url.
    Function mocked in unit tests."
@@ -47,11 +70,12 @@
         (date-parution  (node-selector-to-text ".date_parution" it))
         ;; (href (node-selector-to-text ".titre[href]"))
         )
-    `(title ,titre
-            authors ,auteurs
-            price ,prix
-            editor ,editeur
-            date-publication ,date-parution)))
+    (make-instance 'book
+                   :title titre
+                   :authors auteurs
+                   :price prix
+                   :editor editeur
+                   :date-publication date-parution)))
 
 (defun build-url (query &key (source *datasource*))
   "Build the search url with the query terms in it.
