8fd571c9c80826d1a0c9b005715edbffb0fa2aeb
vindarel <ehvince@mailz.org>
2018-04-10 16:53:53 +0200

    basic DB test

--- tests/test-bookshops.lisp
+++ tests/test-bookshops.lisp
@@ -5,7 +5,12 @@
         :prove)
   (:import-from :bookshops.models
                 :book
-                :title))
+                :make-book
+                :save-book
+                :title
+                :quantity)
+  (:import-from :bookshops-test.utils
+                :with-empty-db))
 (in-package :bookshops-test)
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :bookshops)' in your Lisp.
@@ -17,5 +22,14 @@
     (ok book "creation")
     (is (title book) "Antigone" "title access"))
   )
+
+(subtest "Creation and DB save"
+  (with-empty-db
+    (let ((bk (make-book :title "in-test")))
+      (save-book bk)
+
+      (is (quantity bk)
+          1
+          "The quantity is 1 after adding to the DB."))))
 
 (finalize)
