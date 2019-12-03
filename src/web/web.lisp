000822ba63387edeb219867e6338865e6c486e01
vindarel <vindarel@mailz.org>
2019-12-03 16:54:47 +0100

    web, parenscript: increment/decrement a hidden input, change DOM content

--- src/web/web.lisp
+++ src/web/web.lisp
@@ -51,7 +51,49 @@
   (with-page (:title (title book))
     (:header (:h1 (title book)))
     (:section
-     (:div (:button :onclick (ps (alert "Not implemented")) "Command"))
+     (:div "Command copies:")
+     (:form :action (format nil "/book/~a/command" (mito:object-id book))
+            :method "POST"
+            ;; reminder: spinneret accepts #ids and .classes notations.
+            :id "commandForm"
+            (:p (:button
+                 :type "button"
+                 :onclick (ps
+                            (let ((elt (chain document (get-element-by-id "qty"))))
+                              (incf (chain elt value))
+                              (setf (chain document (get-element-by-id "show-qty") |innerText|)
+                                    (chain elt value))
+                              (chain console (log "new qty: "
+                                                  (chain elt value)))
+                              nil))
+                 "Add 1"))
+            (:p (:button
+            (:span (:button
+                 :type "button"
+                 :onclick (ps
+                            (let* ((elt (chain document (get-element-by-id "qty")))
+                                   (qty (parse-int (@ elt value)))
+                                   (nb (chain document (get-element-by-id "show-qty"))))
+                              (decf (chain elt value))
+                              (setf (@ nb inner-text)
+                                    (chain elt value))
+                              (chain console (log "type of qty" (typeof qty)))
+                              (when (>= qty 79)
+                                (chain console (log "when"))
+                                (setf (@ nb style background-color)
+                                      "red"))
+                              (when (< qty 79)
+                                (setf (@ nb style background-color)
+                                      "white"))
+                              (chain console (log "new qty: "
+                                                  (chain elt value)))
+                              nil))
+                 "Remove 1"))
+
+            (:span (:span "to command: ")
+                   (:span :id "show-qty"))
+            (:input :type "hidden" :name "qty" :id "qty" :value 1)
+            (:p (:input :class "button" :type "submit" :value "Validate")))
      (:span
       (:td (mito:object-id book))
       (:td (title book)))
@@ -64,6 +106,26 @@
 
 (defroute book-details ("/book/:id") ()
   (book-show (find-by :id id)))
+
+(defroute command-book-route ("/book/:id/command" :method :post)
+    (&post qty)
+  (format t "~&--- command for id ~a, qty: ~a~&" id qty)
+  (format t "~&hunchentoot post params: ~a~&" (hunchentoot:post-parameters hunchentoot:*request*))
+  (with-html-string
+    (:div (format nil "qty: ~a" qty))))
+
+(defroute command-book-route ("/book/:id/command" :method :get) ()
+  (format t "~&--- command ~a with GET: do nothing~&" id))
+
+(defroute testroute ("/test") (x y)
+  (format t "~&---- TEST x: ~a, y: ~a~&" x y)
+  (with-html-string
+    (:div (format nil "x: ~a, y: ~a" x y))))
+
+(defroute testroute ("/test" :method :post) (x y)
+  (format t "~&---- POST test x: ~a, y: ~a~&" x y)
+  (with-html-string
+    (:div (format nil "x: ~a, y: ~a" x y))))
 
 (defvar *server* nil
   "Current instance of easy-acceptor.")
