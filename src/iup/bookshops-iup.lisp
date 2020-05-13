bcffd30dfb255ffa0b3d35612cf6d14e289801ed
vindarel <vindarel@mailz.org>
2020-05-13 11:15:57 +0200

    useless IUP tests

--- src/iup/bookshops-iup.lisp
+++ src/iup/bookshops-iup.lisp
@@ -1,4 +1,3 @@
-
 (eval-when (:compile-toplevel)
   (ql:quickload :iup))
 
@@ -20,9 +19,52 @@
                                             (iup:version)
                                             (lisp-implementation-type)
                                             (lisp-implementation-version))))
-           (dialog (iup:dialog label :title "Hello, World!")))
+           (button (iup:button :title "Click me"
+                               :expand :yes
+                               :tip "yes, click me"
+                               :action (lambda (handle)
+                                         (declare (ignorable handle))
+                                         (iup:message "title" "button clicked")
+                                         iup:+default+)))
+           (vbox
+            (iup:vbox (list label button)
+                      :gap "10"
+                      :margin "10x10"
+                      :alignment :acenter))
+           (dialog (iup:dialog vbox :title "Hello, World!")))
       (iup:show dialog)
       (iup:main-loop))))
+
+(defun counter ()
+  (iup:with-iup ()
+    (let* ((counter (iup:label :title 0))
+           (label (iup:label :title (format nil "The button was clicked ~a time(s)."
+                                            (iup:attribute counter :title))))
+           (button (iup:button :title "Click me"
+                               :expand :yes
+                               :tip "yes, click me"
+                               :action (lambda (handle)
+                                         (declare (ignorable handle))
+                                         (setf (iup:attribute counter :title)
+                                               (1+ (iup:attribute counter :title 'number)))
+                                         (setf (iup:attribute label :title)
+                                               (format nil "The button was clicked ~a times."
+                                                       (iup:attribute counter :title)))
+                                         iup:+default+)))
+           (vbox
+            (iup:vbox (list label button)
+                      :gap "10"
+                      :margin "10x10"
+                      :alignment :acenter))
+           (dialog (iup:dialog vbox :title "Counter")))
+      (iup:show dialog)
+      (iup:main-loop))))
+
+(defun run-counter ()
+  #+sbcl
+  (sb-int:with-float-traps-masked
+      (:divide-by-zero :invalid)
+    (counter)))
 
 (defun gridexample ()
   (iup:with-iup ()
@@ -81,6 +123,44 @@
       (iup:map dialog)
       (iup:show dialog)
       (iup:main-loop))))
+
+(defun list-test ()
+  (iup:with-iup ()
+    (let*  ((list-1 (iup:list :tip "List 1"  ;; tooltip
+                              :expand :yes
+                              ;; multiple selection
+                              :multiple :yes))
+            (list-2 (iup:list :value 2   ;; default index of the selected row
+                              :expand :yes
+                              :tip "List 2"))
+            (list-3 (iup:list :value 9 :tip "List 3" :expand :yes))
+            (frame (iup:frame
+                    (iup:hbox
+                     (progn
+                       ;; display a list of integers.
+                       (loop for i from 1 upto 10
+                          do (setf (iup:attribute list-1 i)
+                                   (format nil "~A" i))
+                          do (setf (iup:attribute list-2 i)
+                                   (format nil "~A" (+ i 10)))
+                          do (setf (iup:attribute list-3 i)
+                                   (format nil "~A" (+ i 50))))
+                       ;; vbox wants a list of widgets.
+                       (list list-1 list-2 list-3)))
+
+                    :title "IUP List"))
+            (dialog (iup:dialog frame :menu "menu" :title "List example")))
+
+      (iup:map dialog)
+      (iup:show dialog)
+      (iup:main-loop))))
+
+(defun run-list-test ()
+  #-sbcl (hello)
+  #+sbcl
+  (sb-int:with-float-traps-masked
+      (:divide-by-zero :invalid)
+    (list-test)))
 
 (defun main ()
   #-sbcl (hello)
