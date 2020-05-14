628a0ab4fb007134683211de014784f44fe4fae4
vindarel <vindarel@mailz.org>
2020-05-14 20:06:58 +0200

    (minor) migrate-all with list of tables

--- src/database.lisp
+++ src/database.lisp
@@ -3,6 +3,14 @@
 ;; DB connection, migrations.
 ;;
 
+(defparameter *tables* '(book
+                         place
+                         place-copies
+                         contact
+                         contact-copies
+                         basket
+                         basket-copies))
+
 (defun connect ()
   "Connect to the DB."
   ;; also use mito:*connection*
@@ -10,16 +18,8 @@
   (setf *db* (connect-toplevel :sqlite3 :database-name *db-name*)))
 
 (defun ensure-tables-exist ()
-  (mapcar #'ensure-table-exists '(book
-                                  place
-                                  place-copies
-                                  contact
-                                  contact-copies)))
+  (mapcar #'ensure-table-exists *tables*))
 
 (defun migrate-all ()
   "Migrate the Book table after we changed the class definition."
-  (mapcar #'mito:migrate-table '(book
-                                 place
-                                 place-copies
-                                 contact
-                                 contact-copies)))
+  (mapcar #'mito:migrate-table *tables*))
