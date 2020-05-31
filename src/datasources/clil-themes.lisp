a13031db7ad77692e69973692b78de117e75b5d4
vindarel <vindarel@mailz.org>
2020-05-31 10:43:49 +0200

    CLIL themes: include code -> name / subname / subsubname mapping
    for public documentation rather than algorithmic consumption.
    
    [ci skip]

--- src/datasources/clil-themes.lisp
+++ src/datasources/clil-themes.lisp
@@ -65,57 +65,67 @@ Generate two files:
       (with-open-file (theme-hierarchies "theme-hierarchies.csv"
                                          :direction :output
                                          :if-exists :supersede)
-        (loop for line in (subseq lines
-                                  (or start 0)
-                                  (or end (length lines)))
-           for columns = (str:split ";" line)
-           ;; Only one theme number is given on each line.
-           for level-1 = (elt columns 0)
-           for level-2 = (elt columns 1)
-           for level-3 = (elt columns 2)
-           for level-4 = (elt columns 3)
-           for name = (elt columns 4)
-           when (not (str:blankp level-1))
-           do (progn (setf current-l1 level-1)
-                     (setf current-l1-name name)
-                     (format t "~%level 1:~t~t~t ~a -> ~a~&" level-1 name)
-                     ;; Save to files.
-                     (format theme-names "~&~a;~a" level-1 name)
-                     ;; Try a yaml representation
-                     (when write-to-files
-                       (format theme-names "~&-~a;~a" level-1 name)
-                       (format theme-hierarchies "~&~a;~a;;;" level-1 level-1)))
+        (with-open-file (theme-hierarchies-names "theme-hierarchies-names.csv"
+                                                 :direction :output
+                                                 :if-exists :supersede)
+          (loop for line in (subseq lines
+                                    (or start 0)
+                                    (or end (length lines)))
+             for columns = (str:split ";" line)
+             ;; Only one theme number is given on each line.
+             for level-1 = (elt columns 0)
+             for level-2 = (elt columns 1)
+             for level-3 = (elt columns 2)
+             for level-4 = (elt columns 3)
+             for name = (elt columns 4)
+             when (not (str:blankp level-1))
+             do (progn (setf current-l1 level-1)
+                       (setf current-l1-name name)
+                       (format t "~%~a: ~a~&" level-1 name)
+                       ;; Save to files.
+                       (format theme-names "~&~a;~a" level-1 name)
+                       ;; Try a yaml representation
+                       (when write-to-files
+                         (format theme-hierarchies-names "~&~a;~a" level-1 name)
+                         (format theme-names "~&-~a;~a" level-1 name)
+                         (format theme-hierarchies "~&~a;~a;;;" level-1 level-1)))
 
-           when (not (str:blankp level-2))
-           do (progn (setf current-l2 level-2)
-                     (setf current-l2-name name)
-                     (format t "~&~tlevel 2:~t~t ~a -> ~a -> ~a~&" level-2 current-l1-name name)
-                     (when write-to-files
-                       (format theme-names "~&~a;~a" level-2 name)
-                       (format theme-hierarchies "~&~a;~a;~a;;" level-2 current-l1 level-2)))
+             when (not (str:blankp level-2))
+             do (progn (setf current-l2 level-2)
+                       (setf current-l2-name name)
+                       (format t "~&~a: ~a / ~a~&" level-2 current-l1-name name)
+                       (when write-to-files
+                         (format theme-hierarchies-names "~&~a: ~a / ~a~&" level-2 current-l1-name name)
+                         (format theme-names "~&~a;~a" level-2 name)
+                         (format theme-hierarchies "~&~a;~a;~a;;" level-2 current-l1 level-2)))
 
-           when (not (str:blankp level-3))
-           do (progn (setf current-l3 level-3)
-                     (setf current-l3-name name)
-                     (format t "~&~t~tlevel 3:~t ~a -> ~a -> ~a -> ~a~&"
-                             level-3
-                             current-l1-name
-                             current-l2-name
-                             name)
-                     (when write-to-files
-                       (format theme-names "~&~a;~a" level-3 name)
-                       (format theme-hierarchies "~&~a;~a;~a;~a;" level-3 current-l1 current-l2 level-3)))
+             when (not (str:blankp level-3))
+             do (progn (setf current-l3 level-3)
+                       (setf current-l3-name name)
+                       (format t "~&~a: ~a / ~a / ~a~&"
+                               level-3
+                               current-l1-name
+                               current-l2-name
+                               name)
+                       (when write-to-files
+                         (format theme-hierarchies-names "~&~a: ~a / ~a / ~a~&"
+                                 level-3
+                                 current-l1-name
+                                 current-l2-name
+                                 name)
+                         (format theme-names "~&~a;~a" level-3 name)
+                         (format theme-hierarchies "~&~a;~a;~a;~a;" level-3 current-l1 current-l2 level-3)))
 
-           when (not (str:blankp level-4))
-           do (progn (setf current-l4 level-4)
-                     (setf current-l4-name name)
-                     (format t "~&~t~t~tlevel 4: ~a -> ~a -> ~a -> ~a -> ~a~&"
-                             level-4
-                             current-l1-name
-                             current-l2-name
-                             current-l3-name
-                             name)
-                     (when write-to-files
-                       (format theme-names "~&~a;~a" level-4 name)
-                       (format theme-hierarchies "~&~a;~a;~a;~a;~a"
-                               level-4 current-l1 current-l2 current-l3 level-4))))))))
+             when (not (str:blankp level-4))
+             do (progn (setf current-l4 level-4)
+                       (setf current-l4-name name)
+                       (format t "~&~a: ~a / ~a / ~a / ~a~&"
+                               level-4
+                               current-l1-name
+                               current-l2-name
+                               current-l3-name
+                               name)
+                       (when write-to-files
+                         (format theme-names "~&~a;~a" level-4 name)
+                         (format theme-hierarchies "~&~a;~a;~a;~a;~a"
+                                 level-4 current-l1 current-l2 current-l3 level-4)))))))))
