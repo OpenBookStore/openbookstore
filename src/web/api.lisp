261d8bef7aa3450578922ef1cd87a6228f37afc5
Gnuxie <gnuxie@protonmail.com>
2020-06-24 20:23:21 +0100

    clean up api-card-add/remove-one routes

--- src/web/api.lisp
+++ src/web/api.lisp
@@ -6,14 +6,16 @@
 (defroute api-card-add-one ("/api/card/add" :method :post)
     (&post (id :parameter-type 'integer))
   (log:debug "Requested id: ~a.~&" id)
-  (let ((card (mito:find-dao 'book :id id)))
-    (when card
-      (bookshops.models:add-to (bookshops.models:default-place) card))))
+  (let ((card (bookshops.models:find-by :id id)))
+    (assert card)
+    (princ-to-string
+     (bookshops.models:add-to (bookshops.models:default-place) card))))
 
 (defroute api-card-remove-one ("/api/card/remove" :method :post)
     (&post (id :parameter-type 'integer))
-  (let ((card (mito:find-dao 'book :id id)))
-    (when card
-      (bookshops.models:add-to (bookshops.models:default-place)
-                               card
-                               :quantity -1))))
+  (let ((card (bookshops.models:find-by :id id)))
+    (assert card)
+    (princ-to-string
+     (bookshops.models:add-to (bookshops.models:default-place)
+                              card
+                              :quantity -1))))
