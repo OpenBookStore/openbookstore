3c817b4417bbfbc247279d94550e0a4aacaf67f4
vindarel <vindarel@mailz.org>
2024-02-21 13:38:44 +0100

    mito-admin: create form with related column

--- src/web/mito-admin/mito-forms.lisp
+++ src/web/mito-admin/mito-forms.lisp
@@ -218,16 +218,73 @@ ok!
 (defparameter *catch-errors* nil
   "Set to T to not have the debugger on an error. We could use hunchentoot's *catch-errors*.")
 
+(defun params-symbols-alist (params)
+ (loop for (field . val) in params
+       collect (cons (alexandria:symbolicate (str:upcase field)) val)))
+
+(defun params-keywords-alist (params)
+ (loop for (field . val) in params
+       collect (cons (alexandria:make-keyword field) val)))
+
+(defun params-keywords (params)
+  (params-keywords-alist (params-symbols-alist params)))
+
+
+(defun field-is-related-column (field)
+  "The Mito table-column-type of this field (symbol) is a DB table (listed in our *tables*).
+
+  Return: field symbol or nil."
+  (let* ((slot (mito.class:find-slot-by-name 'book (alexandria:symbolicate (str:upcase field))))
+         (column-type (and slot (mito.class:table-column-type slot))))
+    (find column-type (tables))))
+#++
+(field-is-related-column 'shelf)
+;; SHELF
+#++
+(field-is-related-column 'title)
+;; NIL
+
+(defun replace-related-objects (params)
+  "If a parameter is a related column, find the object by its ID and put it in the PARAMS alist.
+  Change the alist in place.
+
+  This is done before the form validation.
+
+  PARAMS: alist of keywords (string) and values.
+
+  Return: params, modified."
+  (loop for (field . val) in params
+        for slot = (mito.class:find-slot-by-name 'book (alexandria:symbolicate (str:upcase field)))
+        for col = (and slot (mito.class:table-column-type slot))
+        for col-obj = nil
+        if (find col *tables*)
+          do (setf col-obj (mito:find-dao col :id val))
+             (log:debug "~a is a related column: ~s" field col-obj)
+             (setf (cdr (assoc field params :test #'equal))
+                   col-obj))
+  params)
+
+#+test-openbookstore
+(let ((params (replace-related-objects '(("TITLE" . "test") ("SHELF" . "1")))))
+  ;; => (("TITLE" . "test") ("SHELF" . #<SHELF 1 - Histoire>))
+  (assert (not (equal (cdr (assoc "SHELF" params :test #'equal))
+                      "1"))))
+
+
 (defgeneric save-record (table &key params &allow-other-keys)
   (:documentation "Process POST paramaters, create a new record or return a form with errors.
 
     Return a hash-table to tell the route what to do: render a template or redirect.")
   (:method (table &key params &allow-other-keys)
     (log:info params)
+    (setf params (replace-related-objects params))
+    (log:info "params with relations: ~a" params)
+
     (let* ((form (make-form table))
            (fields (form-fields form))
            (inputs (collect-slot-inputs form fields))
            (keywords (params-keywords params))
+           (params-symbols-alist (params-symbols-alist params))
            (record nil)
            (model (slot-value form 'model))
            (errors nil))
